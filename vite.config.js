9b985d69126531a42ad5abec3b5a671450b6cb5f
David <jdvp736@gmail.com>
2025-11-30 22:12:35 -0600

    Probando otra vez

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // 👇 IMPORTANTE: La base debe apuntar al nombre del repositorio EXACTO
   base: "/Pokedex/",
-
   plugins: [
     tailwindcss(),
     react()
   ],
-
-  // 👇 Opcional pero recomendado para GitHub Pages
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  }
 })
